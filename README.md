3e0987aaa4534c43eb7937ae6f5da7c2c9b079c1
vivica007 <vitoriaalvisoliveira1810@gmail.com>
2025-06-26 19:55:21 -0400

    Primeiro commit - Cálculo da esfera

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cálculo de Área e Volume de uma Esfera em PHP
+
+Scripts PHP para calcular a área e o volume de uma esfera.
---
+++ esfera.php
@@ -0,0 +1,11 @@
+<?php
+const PI = 3.141592;
+
+$raio = 5;
+
+$area = 4 * PI * ($raio * $raio);
+$volume = (4/3) * PI * ($raio * $raio * $raio);
+
+echo "Área da esfera: " . $area . " metros quadrados.<br>";
+echo "Volume da esfera: " . $volume . " metros cúbicos.";
+?>
